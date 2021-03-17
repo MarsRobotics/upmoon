5ae0e5dfc981b77fd30bf28bb9755ecdd90a437c
Kevin Hoser <hoser21@up.edu>
2021-03-17 20:35:45 +0000

    Add pack in and pack out

--- upmoon_teleop/scripts/teleop_joy.py
+++ upmoon_teleop/scripts/teleop_joy.py
@@ -2,17 +2,44 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from sensor_msgs.msg import Joy
+from sensor_msgs.msg import Joy, Float64
 
 class TeleopJoy:
 
     SPEED_LINEAR = 1    # Max velocity for a linear twist vector
     SPEED_ANGULAR = 1   # Max velocity for an angular twist vector
+    ANKLE_PACK_IN = 0
+    ANKLE_PACK_OUT = 3.14159265
 
     def __init__(self):
-        self.pub = rospy.Publisher('/simple4wd/mobile_base_controller/cmd_vel', Twist, queue_size=10)
-        rospy.Subscriber('joy', Joy, self.joy_callback)
+        self.pub_twist              = rospy.Publisher('/diff_drive_controller/cmd_vel', Twist, queue_size=10)
         
+        self.pub_excavate_spin      = rospy.Publisher('/motor/excavate_spin', Float64, queue_size=10)
+        self.pub_excavate_actuator  = rospy.Publisher('/motor/excavate_actuator', Float64, queue_size=10)
+        self.pub_depositor_actuator = rospy.Publisher('/motor/depositor_actuator', Float64, queue_size=10)
+        self.pub_ankle_lf           = rospy.Publisher('/ankle_lf_controller/command', Float64, queue_size=10)
+        self.pub_ankle_lm           = rospy.Publisher('/ankle_lm_controller/command', Float64, queue_size=10)
+        self.pub_ankle_lb           = rospy.Publisher('/ankle_lb_controller/command', Float64, queue_size=10)
+        self.pub_ankle_rf           = rospy.Publisher('/ankle_rf_controller/command', Float64, queue_size=10)
+        self.pub_ankle_rm           = rospy.Publisher('/ankle_rm_controller/command', Float64, queue_size=10)
+        self.pub_ankle_rb           = rospy.Publisher('/ankle_rb_controller/command', Float64, queue_size=10)
+
+        rospy.Subscriber('joy', Joy, self.joy_callback)
+    
+
+    """
+    Set all A joints to a radian
+    """
+    def ankle_set(self, rad):
+        payload = Float64()
+        payload.data = rad
+        self.pub_ankle_lf.publish(payload)
+        self.pub_ankle_lm.publish(payload)
+        self.pub_ankle_lb.publish(payload)
+        self.pub_ankle_rf.publish(payload)
+        self.pub_ankle_rm.publish(payload)
+        self.pub_ankle_rb.publish(payload)
+
 
     """
     Example Twist Message:
@@ -60,9 +87,18 @@ class TeleopJoy:
         # twist.linear.x = total_speed * rs_x
         # twist.linear.y = total_speed * rs_y
         
+        if (joy_msg.buttons[15] and joy_msg.buttons[16]):
+            # ignore as both are pressed
+            pass
+        elif (joy_msg.buttons[15]):
+            # left d pad was pressed => pack out
+            self.ankle_set(self.ANKLE_PACK_OUT)
+        elif (joy_msg.buttons[16]):
+            # right d pad was pressed => pack in
+            self.ankle_set(self.ANKLE_PACK_IN)
 
         # Publish twist message
-        self.pub.publish(twist)
+        self.pub_twist.publish(twist)
 
     def normalize_trigger(self, trig):
         trig = float(trig)
